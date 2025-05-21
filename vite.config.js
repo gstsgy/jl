d0195c25528453e2872551275aa37597d112ddf2
122759654 <122759654@qq.com>
2025-05-21 02:54:34 +0000

    init

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root:'src',
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      allowedHosts: ['all','5173.app.cloudstudio.work','127.0.0.1','localhost'] // 允许所有的hosts连接到Vite开发服务器
+    }
+  })
+  
